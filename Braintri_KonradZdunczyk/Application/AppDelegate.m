3879084030bee9e28bfa8793a3770f3af38f162d
Konrad Zdunczyk <konrad.zdunczyk@gmail.com>
2016-10-26 15:38:37 +0200

    Cleaning

---
+++ Braintri_KonradZdunczyk/Application/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  Braintri_KonradZdunczyk
+//
+//  Created by Konrad Zdunczyk on 26/10/16.
+//  Copyright © 2016 Konrad Zdunczyk. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
